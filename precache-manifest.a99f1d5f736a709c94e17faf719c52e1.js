0650621b7a58746a73353d6fb27afce01fdb215d
oikuman <oikuma@gmail.com>
2019-10-20 23:31:50 +0300

    Updates

---
+++ precache-manifest.a99f1d5f736a709c94e17faf719c52e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a466cee5aa2ec13fa9f4a113ce690c",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "fb11a59117f6fee7d771",
+    "url": "/goit-react-hw-04-reader/static/css/main.bc6926db.chunk.css"
+  },
+  {
+    "revision": "927e87631f441cbbc8fb",
+    "url": "/goit-react-hw-04-reader/static/js/2.efacfe7f.chunk.js"
+  },
+  {
+    "revision": "fb11a59117f6fee7d771",
+    "url": "/goit-react-hw-04-reader/static/js/main.42eb7a34.chunk.js"
+  },
+  {
+    "revision": "17644c1e8210fcaeab18",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.4a927777.js"
+  }
+]);
